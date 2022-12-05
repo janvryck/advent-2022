bf756272f1468eb3b1eaa30a292a7e9dca510cbf
Jan Van Ryckeghem <jan@tabs-spaces.be>
2022-12-05 11:16:32 +0100

    Day 6 - setup

---
+++ src/main/kotlin/be/tabs_spaces/advent2022/days/Day06.kt
@@ -0,0 +1,11 @@
+package be.tabs_spaces.advent2022.days
+
+class Day06: Day(6) {
+    override fun partOne(): Any {
+        return -1
+    }
+
+    override fun partTwo(): Any {
+        return -1
+    }
+}
---
+++ src/test/kotlin/be/tabs_spaces/advent2022/days/Day06Test.kt
@@ -0,0 +1,18 @@
+package be.tabs_spaces.advent2022.days
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day06Test {
+    private val day06 = Day06()
+
+    @Test
+    fun partOne() {
+        assertThat(day06.partOne()).isEqualTo(-1)
+    }
+
+    @Test
+    fun partTwo() {
+        assertThat(day06.partTwo()).isEqualTo(-1)
+    }
+}
