bcedc4d57769d9d202dcfd3c3bf3e30a4595ee02
Jan Van Ryckeghem <jan@tabs-spaces.be>
2022-12-05 10:52:41 +0100

    expression body

--- src/main/kotlin/be/tabs_spaces/advent2022/days/Day03.kt
+++ src/main/kotlin/be/tabs_spaces/advent2022/days/Day03.kt
@@ -4,19 +4,15 @@ private const val alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWX
 
 class Day03 : Day(3) {
 
-    override fun partOne(): Any {
-        return inputList
-            .map { it.chunked(it.length/2) }
-            .flatMap { (first, second) -> first.toSet().intersect(second.toSet()) }
-            .sumOf { alphabet.indexOf(it) + 1 }
-    }
+    override fun partOne() = inputList
+        .map { it.chunked(it.length/2) }
+        .flatMap { (first, second) -> first.toSet().intersect(second.toSet()) }
+        .sumOf { alphabet.indexOf(it) + 1 }
 
-    override fun partTwo(): Any {
-        return inputList
-            .map { it.toSet() }
-            .chunked(3)
-            .flatMap { (first, second, third) -> first.intersect(second).intersect(third) }
-            .sumOf { alphabet.indexOf(it) + 1 }
-    }
+    override fun partTwo() = inputList
+        .map { it.toSet() }
+        .chunked(3)
+        .flatMap { (first, second, third) -> first.intersect(second).intersect(third) }
+        .sumOf { alphabet.indexOf(it) + 1 }
 
 }
